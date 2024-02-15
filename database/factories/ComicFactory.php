cada5ea2e2188b5a9a6f56e29ca61e6cab9b0de9
TobiaRigon <rigontobia@gmail.com>
2024-02-15 15:43:14 +0100

    Feat: milestone1 : create migration + factory + seeder + model + faker

---
+++ database/factories/ComicFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ComicFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake() -> title,
+            'description'=> fake() ->  paragraph,
+            'author'=> fake() -> name,
+            'price'=> fake() -> randomFloat(2, 5, 100),
+        ];
+    }
+}
