76f660d867bad1671accdc22833b4bdc4e6c913a
TobiaRigon <rigontobia@gmail.com>
2024-02-19 15:48:44 +0100

    Feat: ita error messages in edit

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -92,11 +92,24 @@ class ComicController extends Controller
 
         $data = $request -> validate( [
 
-            'title' => 'required|string|max:255|min:2',
-            'author' => 'required|string|max:255|min:2',
-            'description' => 'string',
-            'price' => 'required|numeric|min:0',
-        ]);
+              'title' => 'required|string|min:2|max:255',
+    'author' => 'required|string|min:2|max:255',
+    'description' => 'nullable|string',
+    'price' => 'required|numeric|min:0.01',
+], [
+    'title.required' => 'Il titolo è obbligatorio.',
+    'title.string' => 'Il titolo deve essere una stringa.',
+    'title.min' => 'Il titolo deve contenere almeno :min caratteri.',
+    'title.max' => 'Il titolo non può superare :max caratteri.',
+    'author.required' => 'L\'autore è obbligatorio.',
+    'author.string' => 'L\'autore deve essere una stringa.',
+    'author.min' => 'L\'autore deve contenere almeno :min caratteri.',
+    'author.max' => 'L\'autore non può superare :max caratteri.',
+    'description.string' => 'La descrizione deve essere una stringa.',
+    'price.required' => 'Il prezzo è obbligatorio.',
+    'price.numeric' => 'Il prezzo deve essere un numero.',
+    'price.min' => 'Il prezzo non può essere inferiore a :min.',
+]);
 
         $data = $request -> all();
 
